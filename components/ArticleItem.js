0f6f4dae9703481bf5bab0a858713c28fd299db5
Amal221200 <amalmurikkoli2000@gmail.com>
2022-02-04 22:58:40 +0530

    Initial Commit

---
+++ components/ArticleItem.js
@@ -0,0 +1,16 @@
+import articleItemStyles from '../styles/Article.module.css';
+import Link from 'next/link';
+
+
+const ArticleItem = ({ article }) => {
+  return(
+    <Link href={`/article/[id]`} as={`/article/${article.id}`}>
+        <a className={articleItemStyles.card}>
+            <h3>{article.title} &rarr;</h3>
+            <p>{article.body}</p>
+        </a>
+    </Link>
+  );
+};
+
+export default ArticleItem;
